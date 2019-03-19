e812a4b7592788c26b10edf77d13b5821451e937
halomoancarel1 <halomoanc@gmail.com>
2019-03-19 20:20:00 +0700

    Update 20190319

---
+++ regularExpressionG.js
@@ -0,0 +1,15 @@
+
+function panggilRegexp() {
+    let data = "Belajar-Satu-Tahun-Bersama-Niomic";
+
+    console.log(data.replace(/-/g,' '))
+}
+
+panggilRegexp();
+//     console.log(str.match(/c/g));
+//     console.log(str.match(/k/g));
+//     console.log(str.match(/l/g));
+//     //fungsi g untuk mencari keseluruhan
+// }
+//
+// panggilRegexp();
